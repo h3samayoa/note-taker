1b4377f53676f893d674108a58ac0640488fb07f
h3samayoa <h3samayoa@gmail.com>
2021-11-01 20:13:38 -0700

    added routing for notes file

--- server.js
+++ server.js
@@ -20,3 +20,36 @@ app.get('/', function(req, res) {
 app.get('/notes', function(req, res) {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
+
+app.route('/api/notes')
+    .get(function(req, res) {
+        res.json(db)
+    })
+
+    .post(function(req, res) {
+        let jsonFile = path.join(__dirname, '/db/db.json');
+        let postNote = req.body;
+
+        let maxId = 99;
+
+        for(let i = 0; i< db.length; i++) {
+            let note = db[i];
+
+            if(note.id > maxId) {
+
+            }
+        }
+
+        note.id = maxId + 1;
+
+        db.push(postNote);
+
+        fs.writeFile(jsonFile, JSON.stringify(db), function(err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Note saved!");
+        })
+
+        res.json(postNote);
+    })
